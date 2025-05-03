f5674f5951083bcd332149e21f8a606cf784c3f8
dparth8421 <dparth388@gmail.com>
2025-05-04 00:43:05 +0530

    Explored multiple route handler

--- app.js
+++ app.js
@@ -1,35 +1,29 @@
 const express = require("express");
 const app = express();
 
-// app.use("/admin", (req, res) => {
-//   //In Route Handler order of execution is important
-//   // If you put this route handler first, it will match all requests to /admin and not reach the next one.
-//   res.send("Hello Admin!");
-// });
-app.get("/admin", (req, res) => {
-  res.send({ firstName: "John", lastName: "Doe" });
-});
-
-app.post("/admin", (req, res) => {
-  res.send("data posted to admin!");
-});
-
-app.get("/admin/:id", (req, res) => {
-  res.send(`Hello Admin ${req.params.id}`);
-});
-
-app.get("/ad?min", (req, res) => {
-  // ? means the previous character is optional
-  res.send("Hello Admin!");
-});
-app.get("/ad*min", (req, res) => {
-  // * means the previous character can be repeated 0 or more times
-  res.send("Hello Admin!");
-});
-app.get("/ad+min", (req, res) => {
-  // + means the previous character can be repeated 1 or more times
-  res.send("Hello Admin!");
-});
+// app.use("/admin",[rh1,rh2],rh3,rh4) you can use an array of middleware functions
+app.use(
+  "/admin",
+  (req, res, next) => {
+    console.log("Admin Middleware");
+    next();
+    //res.send("Admin Route");
+  },
+  (req, res, next) => {
+    console.log("Admin Middleware 2");
+    next();
+  },
+  [
+    (req, res, next) => {
+      console.log("Admin Middleware 3");
+      next();
+    },
+    (req, res) => {
+      console.log("Admin Middleware 4");
+      res.send("Admin Route 4");
+    },
+  ]
+);
 
 app.listen(7777, () => {
   console.log("Server is running on port 7777");
