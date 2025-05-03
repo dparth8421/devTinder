0504822db1ff524a8ffe3e265c6a470949674024
dparth8421 <dparth388@gmail.com>
2025-05-03 23:39:07 +0530

    exploring routes

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ const express = require("express");
 const app = express();
 
 app.use("/admin", (req, res) => {
+  //In Route Handler order of execution is important
+  // If you put this route handler first, it will match all requests to /admin and not reach the next one.
   res.send("Hello Admin!");
 });
-app.use("/dasshboard", (req, res) => {
+app.use("/admin/2", (req, res) => {
+  res.send("Hello Hello Hello!");
+});
+app.use("/dashboard", (req, res) => {
   res.send("Hello dashboard!");
 });
-app.use("/test", (req, res) => {
-  res.send("Hello frpm server!");
+app.use("/dashboard/2", (req, res) => {
+  res.send("Hello from server!");
 });
 
 app.listen(7777, () => {
