69ef60e754cac41a3a9c4f785c0bf198042017e0
dparth8421 <dparth388@gmail.com>
2025-05-04 15:10:21 +0530

    Explored error handling

--- app.js
+++ app.js
@@ -2,17 +2,27 @@ const express = require("express");
 const { adminAuth, userAuth } = require("./Middlewares/auth");
 const app = express();
 
-app.use("/admin", adminAuth);
-app.get("/User", userAuth, (req, res) => {
-  res.send("User Data Sent");
-});
-app.get("/admin/getAllData", (req, res) => {
-  res.send("All data sent successfully!");
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("something went wrong"); // In this case error obj is empty so it will go to /getUserData and throw an error
+  }
 });
 
-app.get("/admin/deleteUser", (req, res) => {
-  res.send("Deleted User Successfully!");
+app.get("/getUserData", (req, res) => {
+  // try {
+  throw new Error("Ergergerd");
+  res.send("User Data Sent");
+  // } catch (err) {
+  //     console.log(err);
+  //     res.status(500).send("error occured");
+  //   }
 });
+
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("something went wrong111"); //In this case the error object is not empty so it will go to /getUserData and throw an error
+  }
+}); //always keep error handling middleware at the end
 
 app.listen(7777, () => {
   console.log("Server is running on port 7777");
