dead1f212df68ed0ae0d415cb866c0d228df7328
dparth8421 <dparth388@gmail.com>
2025-05-13 00:30:04 +0530

    API - get a use by emailID. Feed API - get all users from database

--- src/app.js
+++ src/app.js
@@ -16,6 +16,35 @@ app.post("/signUp", async (req, res) => {
   }
 });
 
+//User
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.findOne({ emailId: userEmail });
+    if (!user) {
+      return res.status(404).send("User not found");
+    } else {
+      res.status(200).send(user);
+    }
+  } catch (err) {
+    res.status(500).send("Error fetching user");
+  }
+});
+
+//Feed api - get all users
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find();
+    if (!users) {
+      return res.status(404).send("No users found");
+    } else {
+      res.status(200).send(users);
+    }
+  } catch (err) {
+    res.status(500).send("Error fetching users");
+  }
+});
+
 connectionDB()
   .then(() => {
     console.log("Database connected successfully");
